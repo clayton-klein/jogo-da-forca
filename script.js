24a6e171869058260ccb499d4b7a0d32eaef99f4
Clayton <csklein87@hotmail.com>
2022-05-27 11:28:48 -0300

    salvando progresso

--- script.js
+++ script.js
@@ -14,6 +14,7 @@
 // - A página deve conter um campo para inserção de texto com a finalidade de adicionar novas palavras ao jogo, e um botão "Adicionar nova palavra".
 
 // HTML elements' selection
+let clickAudio = new Audio("sounds/mixkit-retro-game-notification-212.mp3");
 let hangmanImg = document.querySelector(".hangman-img");
 let tip = document.querySelector(".tip");
 let lettersBox = document.querySelector(".letters-box");
@@ -21,23 +22,12 @@ let wrongLettersBox = document.querySelector(".wrong-letters-box");
 let playBtn = document.querySelector(".start-btn");
 let quitBtn = document.querySelector(".quit-btn");
 let addWordBtn = document.querySelector(".add-word-btn");
-let addWordDialog = document.querySelector(".add-word-dialog");
 let saveAddedWordBtn = document.querySelector(".confirm-added-word-btn");
+let playAgainBtn = document.querySelector(".play-again-btn");
+let addWordDialog = document.querySelector(".add-word-dialog");
 let invalidInputDialog = document.querySelector(".invalid-input-dialog");
 let winDialog = document.querySelector(".win-dialog");
 let loseDialog = document.querySelector(".lose-dialog");
-let playAgainBtn = document.querySelector(".play-again-btn");
-
-// addWordBtn.addEventListener("click", () => {
-//   let clickAudio = new Audio("sounds/mixkit-retro-game-notification-212.mp3");
-//   clickAudio.play();
-//   addWordDialog.showModal();
-
-//   let closeDialogBtn = document.querySelector("#closeAddWordDialogBtn");
-//   closeDialogBtn.addEventListener("click", () => {
-//     addWordDialog.close();
-//   });
-// });
 
 let words = [
   "clayton",
@@ -58,10 +48,11 @@ let wrongLetters = [];
 
 let mistakes = 0;
 
-let regexLowerCase = /[a-z]/;
+let regex = /[a-zA-Z]/;
 
 playBtn.addEventListener("click", drawBoardGame);
 function drawBoardGame() {
+  clickAudio.play();
   hangmanImg.setAttribute("src", "images/forca.webp");
 
   createSecretWord();
@@ -79,7 +70,10 @@ function drawBoardGame() {
   document.querySelector("header").style.marginTop = "6vh";
 }
 
-quitBtn.addEventListener("click", resetGame);
+quitBtn.addEventListener("click", () => {
+  clickAudio.play();
+  setTimeout(resetGame, 650);
+});
 
 function createSecretWord() {
   secretWord = words[Math.floor(Math.random() * words.length)];
@@ -99,7 +93,7 @@ let remainingLetters = secretWord.length;
 
 window.addEventListener("input", checkInput);
 function checkInput(e) {
-  if (!regexLowerCase.test(e.input.value)) {
+  if (!regex.test(e.input.value)) {
     invalidInputDialog.showModal();
   } else {
     e.input.value.toUpperCase();
@@ -117,8 +111,11 @@ function checkInput(e) {
 function writeCorrectLetter(e) {
   let correctSound = new Audio("sounds/mixkit-funny-squeaky-toy-hits-2813.mp3");
   correctSound.play();
-  rightLetters[i] = e.input.value;
+
+  rightLetters[i] = e.input.value.toLowerCase();
+
   remainingLetters--;
+
   return rightLetters;
 }
 
@@ -128,16 +125,31 @@ function youWin() {
       "sounds/mixkit-animated-small-group-applause-523.mp3"
     );
     applause.play();
+
     winDialog.showModal();
+
+    let closeWinDialogBtn = document.querySelector("#closeWinDialogBtn");
+    closeWinDialogBtn.addEventListener("click", () => {
+      winDialog.close();
+    });
+
+    let playAgainBtn = document.querySelector("#playAgainBtn");
+    playAgainBtn.addEventListener("click", () => {
+      resetGame();
+    });
   }
 }
 
 function writeIncorrectLetter(e) {
   let wrongSound = new Audio("sounds/mixkit-boxer-getting-hit-2055.mp3");
   wrongSound.play();
-  wrongLetters = e.input.value.push();
+
+  wrongLetters = e.input.value.toUpperCase().push();
+
   mistakes++;
+
   drawHangman();
+
   return wrongLetters;
 }
 
@@ -153,17 +165,48 @@ function gameOver() {
       "sounds/mixkit-sad-game-over-trombone-471.mp3"
     );
     gameOverTrombone.play();
+
     loseDialog.showModal();
+
+    let loseDialogBtn = document.querySelector("#closeLoseDialogBtn");
+    loseDialogBtn.addEventListener("click", () => {
+      clickAudio.play();
+      loseDialog.close();
+    });
+
+    let playAgainLostBtn = document.querySelector("#playAgainLostBtn");
+    playAgainLostBtn.addEventListener("click", () => {
+      clickAudio.play();
+      resetGame();
+    });
   }
 }
 
+addWordBtn.addEventListener("click", addWord);
 function addWord() {
-  let word = document.querySelector(".word-input").value;
-  if (word != regex.match || word.length > 8) {
-    invalidInputDialog.showModal();
-  } else {
-    words.push(word);
-  }
+  clickAudio.play();
+
+  addWordDialog.showModal();
+
+  let wordInput = document.querySelector(".word-input");
+
+  
+  let saveAddedWordBtn = document.querySelector("#saveAddedWordBtn");
+  saveAddedWordBtn.addEventListener('click', () => {
+    clickAudio.play();
+    if (!regex.test(wordInput) || wordInput.length > 8) {
+      invalidInputDialog.showModal();
+    };
+
+    words.push(wordInput);
+  });
+
+  let closeDialogBtn = document.querySelector("#closeAddWordDialogBtn");
+  closeDialogBtn.addEventListener("click", () => {
+    clickAudio.play();
+    addWordDialog.close();
+  });
+
   return words;
 }
 
